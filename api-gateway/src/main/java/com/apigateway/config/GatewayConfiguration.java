ae06a80112f7b96f9e0757ffe6cd99fbdee159f3
ssr14 <ssr14@tpu.ru>
2024-11-27 08:32:53 +0700

    add gateway

---
+++ api-gateway/src/main/java/com/apigateway/config/GatewayConfiguration.java
@@ -0,0 +1,25 @@
+package com.apigateway.config;
+
+import org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class GatewayConfiguration {
+    @Bean
+    public RouterFunction<ServerResponse> eureka(){
+        return GatewayRouterFunctions
+                .route("eureka-service")
+                .route(
+                        RequestPredicates.path("/eureka"),
+                        HandlerFunctions.http("http://discovery-service:8761")
+                )
+                .before(BeforeFilterFunctions.rewritePath("/eureka", "/"))
+                .build();
+    }
+}
